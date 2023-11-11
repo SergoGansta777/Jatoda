3fdcfa155dcdd6b6da2b1bd6be3546c53daae319
SergoGansta777 <sergo777ser777@gmail.com>
2023-11-11 09:43:55 +0300

    Started implementing api controllers

---
+++ JatodaBackendApi/Program.cs
@@ -0,0 +1,30 @@
+using JatodaBackendApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<JatodaContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
